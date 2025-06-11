064719609c2ab559906e356f0ed1a2a12d4553ff
SHIVAMKASHYAP5073 <122219187+SHIVAMKASHYAP5073@users.noreply.github.com>
2025-06-11 12:11:23 +0530

    Update and rename program10.java to KilometerToMeter.java

--- Day01/KilometerToMeter.java
+++ Day01/KilometerToMeter.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-class program10 {
+class KilometerToMeter {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 		System.out.println("Enter distance in kilometers:");
