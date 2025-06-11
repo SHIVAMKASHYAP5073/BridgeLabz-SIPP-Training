1364c6c285c7f781d4bd89ea4923b07534236483
SHIVAMKASHYAP5073 <122219187+SHIVAMKASHYAP5073@users.noreply.github.com>
2025-06-11 12:10:02 +0530

    Update and rename program8.java to PowerCalculations.java

--- Day01/PowerCalculations.java
+++ Day01/PowerCalculations.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-class program8 {
+class PowerCalculations {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 		System.out.println("Enter base:");
