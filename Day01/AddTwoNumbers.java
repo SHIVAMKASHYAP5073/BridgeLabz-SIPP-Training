beee91f91124b6f0f0368074b5c1ec88364581aa
SHIVAMKASHYAP5073 <122219187+SHIVAMKASHYAP5073@users.noreply.github.com>
2025-06-11 12:02:31 +0530

    Update and rename program2.java to AddTwoNumbers.java

--- Day01/AddTwoNumbers.java
+++ Day01/AddTwoNumbers.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-class program2{
+class AddTwoNumbers{
     public static void main(String[] args){
 		Scanner sc = new Scanner(System.in);
         System.out.println("Enter First Number");
